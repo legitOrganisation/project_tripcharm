9609b77fcfc7bd558c5d78c5355f6591d54d9a7a
Chen-Xuan-Ng <ngchenxuan121@gmail.com>
2025-08-10 21:32:35 +0800

    updated test-console

--- test-console/server.js
+++ test-console/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 808080;
+const PORT = 1027;
 
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
